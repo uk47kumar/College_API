5da5dd5f094e068c3bab3f03286cd1795f382f70
uk47kumar <uk47kumar@gmail.com>
2022-10-01 21:04:25 +0530

    first commit

---
+++ src/main/java/com/example/collegeApi/services/CollegeService.java
@@ -0,0 +1,14 @@
+package com.example.collegeApi.services;
+
+import com.example.collegeApi.entities.College;
+
+import java.util.List;
+
+public interface CollegeService {
+    List<College> getAllColleges();
+
+    College getCollege(long collegeId);
+
+//    College getCollegeName(String collegeName);
+
+}
